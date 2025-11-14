85ddbb4404f88547d4a266ac39e2463e085a0965
NickPham21 <npp002@ucsd.edu>
2025-11-14 11:56:23 -0800

    initial form_error capture implementation

--- form-validation-script.js
+++ form-validation-script.js
@@ -2,11 +2,17 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const comment = document.getElementById('comment');
+const form_errors = document.getElementById('form-errors'); 
 const numCharLabel = document.getElementById('char-count');
 const form = document.querySelector('form');
 const error = document.getElementById('error');
 
-let form_errors = [];
+let form_errors_arr = [];
+
+form.addEventListener('submit', (event) => {
+    form_errors_str = JSON.stringify(form_errors_arr);
+    form_errors.value = form_errors_str;
+});
 
 name.addEventListener('input', () => {
     if (name.validity.valid) {
@@ -57,10 +63,10 @@ comment.addEventListener('input', () => {
 function handleNameError() {
     error.className = 'error-show';
     if (name.validity.valueMissing) {
-        form_errors.push('name: value missing');
+        form_errors_arr.push('name: value missing');
         error.textContent = 'Name is required.';
     } else if (name.validity.patternMismatch) {
-        form_errors.push('name: pattern mismatch');
+        form_errors_arr.push('name: pattern mismatch');
         error.textContent = 'Name can only contain letters and spaces.';
     }
 }
@@ -68,13 +74,13 @@ function handleNameError() {
 function handleEmailError() {
     error.className = 'error-show';
     if (email.validity.valueMissing) {
-        form_errors.push('email: value missing');
+        form_errors_arr.push('email: value missing');
         error.textContent = 'Email is required.';
     } else if (email.validity.typeMismatch) {
-        form_errors.push('email: type mismatch');
+        form_errors_arr.push('email: type mismatch');
         error.textContent = 'Entered value needs to be an email address.';
     } else if (email.validity.patternMismatch) {
-        form_errors.push('email: pattern mismatch');
+        form_errors_arr.push('email: pattern mismatch');
         error.textContent = 'Enter a valid email address (e.g., user@example.com).';
     }
 }
@@ -82,13 +88,13 @@ function handleEmailError() {
 function handlePhoneError() {
     error.className = 'error-show';
     if (phone.validity.patternMismatch) {
-        form_errors.push('phone: pattern mismatch');
+        form_errors_arr.push('phone: pattern mismatch');
         error.textContent = 'Enter a valid 10-digit phone number.';
     }
 }
 
 function handleCommentError() {
     error.className = 'error-show';
-    form_errors.push('comment: character limit exceeded');
+    form_errors_arr.push('comment: character limit exceeded');
     error.textContent = `Comment cannot exceed ${comment.maxLength} characters.`;
 }
