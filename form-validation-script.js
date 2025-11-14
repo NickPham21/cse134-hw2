e91eb32726ddce87cf2c7b4e228a02cb2b6146da
NickPham21 <npp002@ucsd.edu>
2025-11-13 22:15:46 -0800

    added warning functionality for nearing character limit in textarea

--- form-validation-script.js
+++ form-validation-script.js
@@ -1,6 +1,7 @@
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
+const comment = document.getElementById('comment');
 const form = document.querySelector('form');
 const errorName = document.getElementById('error-name');
 const errorEmail = document.getElementById('error-email');
@@ -16,7 +17,23 @@ name.addEventListener('input', () => {
     }
 });
 
-
+comment.addEventListener('input', () => {
+    numChar = comment.value.length;
+    limit = comment.maxLength;
+    if (numChar == limit) {
+        // User has reached character limit, show the error
+        comment.className = 'at-character-limit';
+        showCommentError();
+    } else if (numChar > limit-10 && numChar < limit) {
+        // Warn user they are near the character limit
+        comment.className = 'near-char-limit';
+    
+    } else {
+        comment.className = '';
+        errorComment.textContent = '';
+        errorComment.className = 'error-hide';
+    }
+});
 
 function showNameError() {
     errorName.className = 'error-show';
@@ -27,4 +44,9 @@ function showNameError() {
     } else if (name.validity.patternMismatch) {
         errorName.textContent = 'Name can only contain letters and spaces.';
     }
+}
+
+function showCommentError() {
+    errorComment.className = 'error-show';
+    errorComment.textContent = `Comment cannot exceed ${comment.maxLength} characters.`;
 }
